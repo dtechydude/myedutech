0b1a8e93ea9fb890f18254f90a047a1d99824a0a
“Gbenga <dtechydude@gmail.com>
2025-12-26 22:34:00 +0100

    general_updates

--- users/urls.py
+++ users/urls.py
@@ -4,9 +4,12 @@ from django.conf.urls.static import static
 from users import views as user_views 
 from django.contrib.auth import views as auth_views
 from django.urls import reverse_lazy
+from curriculum.models import SchoolIdentity
 
 
 # app_name ='users'
+# We fetch the school data once here or inside the view
+school_data = SchoolIdentity.objects.first()
 
 urlpatterns = [
     path('', auth_views.LoginView.as_view(template_name='users/login.html'), name="login"),
@@ -27,9 +30,17 @@ urlpatterns = [
 
     
     # path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name="password_reset"),
-    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html', success_url = reverse_lazy('users:password_reset_done')), name="password_reset"),
+    # path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html', success_url = reverse_lazy('password_reset_done')), name="password_reset"),
+    path('password-reset/', 
+     auth_views.PasswordResetView.as_view(
+         template_name='users/password_reset.html',
+         email_template_name='registration/password_reset_email_text.txt', # Text version
+         html_email_template_name='registration/password_reset_email.html', # HTML version
+         extra_email_context={'school_info': school_data} # School Information
+     ), 
+     name='password_reset'),
     path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name="password_reset_done"),
-    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html', success_url = reverse_lazy('users:login')), name="password_reset_confirm"),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html', success_url = reverse_lazy('login')), name="password_reset_confirm"),
     path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name="password_reset_complete"),
 
 
